86eb75ba46178fde4f84b4553a50532211fe1712
Ahmed Sabbour <103856+sabbour@users.noreply.github.com>
2019-02-12 19:09:12 -0800

    Revert "Enabled trust proxy"
    This reverts commit 1ad396a07555d0d4afb2769cdedef9c15b2b403d.

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.enable('trust proxy');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
